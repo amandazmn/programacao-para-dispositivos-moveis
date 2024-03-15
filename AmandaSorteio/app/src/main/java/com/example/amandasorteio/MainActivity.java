e1cc0e6e284bea0a6a70715cf58910f255da918e
Amanda <amanda.z1@aluno.ifsc.edu.br>
2024-03-15 11:02:48 -0300

    Sorteador

--- AmandaSorteio/app/src/main/java/com/example/amandasorteio/MainActivity.java
+++ AmandaSorteio/app/src/main/java/com/example/amandasorteio/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.amandasorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtViewResult;
+    EditText edMin, edMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edMin = findViewById(R.id.edMin);
+        edMax = findViewById(R.id.edMax);
+        txtViewResult = findViewById(R.id.textView);
     }
+
+    public void sortear(View v){
+        int min = Integer.parseInt(edMin.getText().toString());
+        int max = Integer.parseInt(edMax.getText().toString());
+
+        Random random = new Random();
+        int result = random.nextInt(max - min + 1) + min;
+
+        txtViewResult.setText(Integer.toString(result));
+    }
+
 }
