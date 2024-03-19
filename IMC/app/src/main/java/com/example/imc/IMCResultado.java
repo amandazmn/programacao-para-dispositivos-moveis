9b804de41ac19ef795910364acd47a33cdb327ce
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2024-03-19 11:52:00 -0300

    IMC

--- IMC/app/src/main/java/com/example/imc/IMCResultado.java
+++ IMC/app/src/main/java/com/example/imc/IMCResultado.java
@@ -5,21 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class IMCResultado extends AppCompatActivity {
 
     TextView txtResultado;
+    ImageView imageViewResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imcresultado);
         txtResultado=findViewById(R.id.textViewResultado);
+        imageViewResultado=findViewById(R.id.imageViewResultado);
 
+        imageViewResultado.setImageResource(R.drawable.abaixopeso);
         Intent i=getIntent();
         Bundle bundle = i.getExtras();
         Log.d("Valor do IMC: ", bundle.getString("valorimc"));
 
         txtResultado.setText(bundle.getString("valorimc"));
+        Double d = bundle.getDouble("valorimc");
+
+        //if()
     }
 }
