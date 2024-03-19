d44a99a0a1e97134a351ece84993d934e48cfe2c
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2024-03-19 09:10:23 -0300

    IMC

--- IMC/app/src/main/java/com/example/imc/MainActivity.java
+++ IMC/app/src/main/java/com/example/imc/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtPeso, txtAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtPeso=findViewById(R.id.txtPeso);
+        txtAltura=findViewById(R.id.txtAltura);
+    }
+
+    public void calcular(View view){
+        double altura = Double.parseDouble(txtAltura.getText().toString());
+        double peso = Double.parseDouble(txtPeso.getText().toString());
+
+        double imc = peso/(altura * altura);
+
+
     }
 }
