4c9e8166057428cd21c435c8a8a2312453fca917
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2024-03-19 11:13:19 -0300

    IMC

---
+++ IMC/app/src/main/java/com/example/imc/IMCResultado.java
@@ -0,0 +1,25 @@
+package com.example.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class IMCResultado extends AppCompatActivity {
+
+    TextView txtResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcresultado);
+        txtResultado=findViewById(R.id.textViewResultado);
+
+        Intent i=getIntent();
+        Bundle bundle = i.getExtras();
+        Log.d("Valor do IMC: ", bundle.getString("valorimc"));
+
+        txtResultado.setText(bundle.getString("valorimc"));
+    }
+}
--- IMC/app/src/main/java/com/example/imc/MainActivity.java
+++ IMC/app/src/main/java/com/example/imc/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.imc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +27,15 @@ public class MainActivity extends AppCompatActivity {
         double altura = Double.parseDouble(txtAltura.getText().toString());
         double peso = Double.parseDouble(txtPeso.getText().toString());
 
-        double imc = peso/(altura * altura);
+        double imcD = peso/(altura * altura);
 
+        DecimalFormat df = new DecimalFormat("##.##");
 
+        String imc = df.format(imcD);
+
+        Toast.makeText(this, imc, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), IMCResultado.class);
+        intent.putExtra("valorimc", imc);
+        startActivity(intent);
     }
 }
