37bd2feee652e270dbf012c4803e81aec028c73e
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2024-04-05 11:51:42 -0300

    Listagem

---
+++ Listagem/app/src/main/java/com/example/listagem/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.listagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] nomes = new String[]{"Amanda", "Lara", "Duda", "Gabi", "Gaby", "Vitória"};
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.listView);
+        //Configurando um adaptador para listagem de dados
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+        );
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("clickitem", Integer.toString(i));
+            }
+        });
+    }
+}
