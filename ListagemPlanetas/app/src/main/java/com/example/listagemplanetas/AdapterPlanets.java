6c3810076151052529fa774c00a90336d102c927
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2024-04-26 11:41:53 -0300

    ListagemPlanetas

--- ListagemPlanetas/app/src/main/java/com/example/listagemplanetas/AdapterPlanets.java
+++ ListagemPlanetas/app/src/main/java/com/example/listagemplanetas/AdapterPlanets.java
@@ -5,21 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class AdapterPlanets extends ArrayAdapter<> {
-    public AdapterPlanets(Context context, int resource, Object[] objects){
+public class AdapterPlanets extends ArrayAdapter<Planet> {
+    Object[] mObjects;
+    public AdapterPlanets(Context context, int resource, Planet[] objects){
         super(context, resource, objects);
+        mObjects = objects;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.item_lista_planeta, null);
-        TextView tv = itemlista.findViewById(R.id.textViewNomePlaneta);
-        Planet
+        View itemLista = LayoutInflater.from(getContext()).inflate(R.layout.item_lista_planeta, parent, false);
+        TextView tvNome = itemLista.findViewById(R.id.textViewNomePlaneta);
+        ImageView ivPlanet = itemLista.findViewById(R.id.imageView2);
+        Planet planet = (Planet) mObjects[position];
+
+        tvNome.setText(planet.nome);
+        ivPlanet.setImageResource(planet.img);
+
+        return itemLista;
     }
 }
--- ListagemPlanetas/app/src/main/java/com/example/listagemplanetas/MainActivity.java
+++ ListagemPlanetas/app/src/main/java/com/example/listagemplanetas/MainActivity.java
@@ -12,17 +12,24 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
 
     String [] planets = new String[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    Planet[] planets = new Planet[]{
-            new Planet("Mercury", mercury.png )
+    Planet[] planet = new Planet[]{
+            new Planet("Mercury", R.drawable.mercury ),
+            new Planet("Venus", R.drawable.venus),
+            new Planet("Earth", R.drawable.terra),
+            new Planet("Mars", R.drawable.mars),
+            new Planet("Jupter", R.drawable.jupiter),
+            new Planet("Saturn", R.drawable.saturn),
+            new Planet("Uranus", R.drawable.uranus),
+            new Planet("Neptune", R.drawable.neptune)
     };
-    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv=findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, planets);
+        ListView lv=findViewById(R.id.listView);
+        //* ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, planets);
+        AdapterPlanets adapter = new AdapterPlanets(this, R.layout.item_lista_planeta, planet);
         lv.setAdapter(adapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
