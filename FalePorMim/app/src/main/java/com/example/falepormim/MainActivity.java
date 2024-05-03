a13570072e29c6e5d830137f1beac968b76e860c
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2024-05-03 11:50:35 -0300

    FalePorMim

---
+++ FalePorMim/app/src/main/java/com/example/falepormim/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.falepormim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TTS tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.edText);
+        button=findViewById(R.id.button);
+        tts = new TTS(this);
+        button.setOnClickListener(v->{
+            String s = editText.getText().toString();
+            tts.speak(s);
+        });
+    }
+}
